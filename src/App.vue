d9d249454ae2e35856445f795b824452146d49b4
natthanguilhot <72070680+natthanguilhot@users.noreply.github.com>
2022-01-04 11:35:39 +0100

    ajout nav + stacks

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <header class="bg-gray-300">
-    <div class="mx-auto px-8 sm:flex max-w-screen-xl">
+    <!-- <nav :class="[isScrolling ? 'bg-gray-300 fixed' : 'bg-white']" class="text-2xl w-full mx-auto z-50 transition-all">
+      <ul class="flex justify-around items-center max-w-screen-xl lg:mx-auto">
+        <li :class="[isScrolling ? 'bg-white' : 'bg-gray-300']" class="py-4 px-2 rounded-lg m-2 sm:w-3/12 max-w-[200px] text-center invisible sm:visible"><a class="h-full w-full" href="#profil">Moi</a></li>
+        <li :class="[isScrolling ? 'bg-white' : 'bg-gray-300']" class="py-4 px-2 rounded-lg m-2 sm:w-3/12 max-w-[200px] text-center invisible sm:visible"><a class="h-full w-full" href="#howiwork">Ce que je fais</a></li>
+        <li :class="[isScrolling ? 'bg-white' : 'bg-gray-300']" class="py-4 px-2 rounded-lg m-2 sm:w-3/12 max-w-[200px] text-center invisible sm:visible"><a class="h-full w-full" href="#mywork">Mon travail</a></li>
+      </ul>
+    </nav> -->
+    <div :class="[isScrolling ? 'sm:pt-40': '']" class="mx-auto px-8 sm:flex max-w-screen-xl sm:pt-20">
       <div class="flex flex-col justify-center items-start text-left font-bold sm:w-6/12">
         <div class="h-20 w-20 bg-orange-600 rounded-full my-6"></div>
         <h1 class="text-5xl lg:text-7xl">Je suis Natthan GUILHOT</h1>
@@ -14,7 +21,7 @@
   </header>
   <main>
 
-    <section class="text-left px-8 text-lg mb-40 max-w-screen-xl lg:mx-auto">
+    <section id="profil" class="text-left px-8 text-lg mb-40 max-w-screen-xl lg:mx-auto">
       <div class="my-8 sm:text-center">
         <h2 class="text-4xl font-bold">PROFIL</h2>
         <p class="my-8">Je suis un développeur Vue.js</p>
@@ -36,9 +43,9 @@
             <p>Bouches-du-Rhone, France</p>
           </div>
           <div class="flex justify-start items-center space-x-4 my-8">
-            <a aria-label="Lien vers mon Linkedin" target="_blank" rel="noopener" href="https://www.linkedin.com/in/natthanguilhot" class="text-5xl text-[#007BB5]"><i class="fab fa-linkedin">LinkedIn</i></a>
-            <a aria-label="Lien vers mon adresse mail" href="mailto:natthan.dev@gmail.com" class="text-5xl text-[#db4a39]"><i class="fas fa-envelope">Mail</i></a>
-            <a aria-label="Lien vers mon profil Malt" href="https://www.malt.fr/profile/natthanguilhot?overview=true" target="_blank" rel="noopener"><img src="./assets/malt-red.svg" alt="Profil Malt" class="h-10 w-10"></a>
+            <a aria-label="Lien vers mon Linkedin" target="_blank" rel="noopener" href="https://www.linkedin.com/in/natthanguilhot" class="text-5xl text-[#007BB5]"><i class="fab fa-linkedin"></i></a>
+            <a aria-label="Lien vers mon adresse mail" href="mailto:natthan.dev@gmail.com" class="text-5xl text-[#db4a39]"><i class="fas fa-envelope"></i></a>
+            <a aria-label="Lien vers mon profil Malt" href="https://www.malt.fr/profile/natthanguilhot?overview=true" target="_blank" rel="noopener"><img src="./assets/malt-red.svg" alt="Profil Malt" class="h-20 w-20"></a>
           </div>
         </div>
       </div>
@@ -58,7 +65,7 @@
       <p class="lg:mt-2"> Durant mon année chez VR'TIG.0 j'ai effectué d'une part le développement des sites de l'entreprise. Et d'autre part le développement d'applications pour les professionnels du secours et du soin d'urgence pour un client ( pharmacie, pompier, ambulance, …). Le développement d’application a beaucoup été formateur notamment par rapport au développement en groupe. Ce qui m’a permis de consolider et améliorer mes connaissances en front-end également. De par cette alternance, j'ai obtenu un diplôme de développeur web reconnue RNCP bac +2. Pour parler un peu plus de moi, je suis passionné par l'informatique et le numérique de manière plus générale. Je regarde et suis de près l'esport. Enfin, je lis beaucoup de livre, surtout des mangas.</p>
     </section>
 
-    <section class="bg-gray-300 py-8">
+    <section id="howiwork" class="bg-gray-300 py-8">
       <div class="text-center px-8 max-w-screen-xl lg:mx-auto">
         <h2 class="text-4xl font-bold my-4">CE QUE JE FAIS</h2>
         <p>En tant que développeur full-stack, je suis amené à travailler sur beaucoup de choses.</p>
@@ -88,7 +95,7 @@
           </div>
           <div class="ml-4 w-9/12">
             <h3 class="text-2xl font-bold mb-4">TAILWINDCSS</h3>
-            <p>Vue est un framework évolutif pour construire des interfaces utilisateur. Vue a été conçu et pensé pour pouvoir être adopté de manière incrémentale.</p>
+            <p>Un framework CSS contenant des classes, et qui peut être composé pour créer n'importe quel design, directement dans le balisage HTML.</p>
           </div>
         </div>
         <div class="flex my-8 w-full min-h-[200px] sm:w-6/12">
@@ -97,7 +104,7 @@
           </div>
           <div class="ml-4 w-9/12">
             <h3 class="text-2xl font-bold mb-4">SASS</h3>
-            <p>Vue est un framework évolutif pour construire des interfaces utilisateur. Vue a été conçu et pensé pour pouvoir être adopté de manière incrémentale.</p>
+            <p>Sass est le langage d'extension CSS de qualité professionnelle le plus mature, stable et puissant  au monde.</p>
           </div>
         </div>
         <div class="flex my-8 w-full min-h-[200px] sm:w-6/12">
@@ -106,7 +113,7 @@
           </div>
           <div class="ml-4 w-9/12">
             <h3 class="text-2xl font-bold mb-4">BACK-END</h3>
-            <p>Vue est un framework évolutif pour construire des interfaces utilisateur. Vue a été conçu et pensé pour pouvoir être adopté de manière incrémentale.</p>
+            <p>Le back-end est la maîtrise toute la partie invisible pour l’utilisateur, mais qui va permettre le bon fonctionnement d’un site internet</p>
           </div>
         </div>
         <div class="flex my-8 w-full min-h-[200px] sm:w-6/12">
@@ -115,7 +122,7 @@
           </div>
           <div class="ml-4 w-9/12">
             <h3 class="text-2xl font-bold mb-4">NODE.JS</h3>
-            <p>Vue est un framework évolutif pour construire des interfaces utilisateur. Vue a été conçu et pensé pour pouvoir être adopté de manière incrémentale.</p>
+            <p>Node.js est un environnement d'exécution JavaScript open source et multiplateforme.</p>
           </div>
         </div>
         <div class="flex my-8 w-full min-h-[200px] sm:w-6/12">
@@ -124,7 +131,7 @@
           </div>
           <div class="ml-4 w-9/12">
             <h3 class="text-2xl font-bold mb-4">MONGODB</h3>
-            <p>Vue est un framework évolutif pour construire des interfaces utilisateur. Vue a été conçu et pensé pour pouvoir être adopté de manière incrémentale.</p>
+            <p>MongoDB est un système de gestion de base de données orienté documents, répartissable sur un nombre quelconque d'ordinateurs et ne nécessitant pas de schéma prédéfini des données.</p>
           </div>
         </div>
         <div class="flex my-8 w-full min-h-[200px] sm:w-6/12">
@@ -133,13 +140,13 @@
           </div>
           <div class="ml-4 w-9/12">
             <h3 class="text-2xl font-bold mb-4">MYSQL</h3>
-            <p>Vue est un framework évolutif pour construire des interfaces utilisateur. Vue a été conçu et pensé pour pouvoir être adopté de manière incrémentale.</p>
+            <p>MySQL est un système de gestion de bases de données relationnelles.</p>
           </div>
         </div>
       </div>
     </section>
 
-    <section class="bgafter relative z-10 py-8 my-16 sm:relative max-w-screen-xl lg:mx-auto">
+    <section id="mywork" class="bgafter relative z-10 py-8 my-16 sm:relative max-w-screen-xl lg:mx-auto">
       <div class="px-8 sm:flex sm:flex-wrap sm:space-y-8">
         <div class="my-8 sm:w-5/12 sm:mx-auto lg:w-4/12">
           <a href="https://vrtig0.fr" target="_blank" rel="noopener">
@@ -169,7 +176,7 @@
       </div>
       <div class="px-8 bg-white h-auto py-4 sm:w-8/12 lg:w-[450px] sm:my-8 sm:translate-x-52 sm:-translate-y-20 sm:pr-0 lg:translate-x-[45rem]">
         <h2 class="text-4xl font-bold py-8">Mon travail</h2>
-        <p class="py-8">Vous pouvez retrouver ici, toutes les missions qui sont accessible à tous. J'ai cependant réalisé d'autres projets qui ne sont pas forcément affichés car non ouvert au public</p>
+        <p class="py-8">Vous pouvez retrouver ici, toutes les missions qui sont accessible à tous. J'ai cependant réalisé d'autres projets qui ne sont pas forcément affichés car non ouvert au public.</p>
         <a href="#contactForm" class="py-4 px-8 border border-black border-2 bg-white my-8 block w-max font-bold text-2xl hover:text-white">Me contacter</a>
       </div>
     </section>
@@ -185,7 +192,7 @@
     </div>
   </section>
 
-  <section class="bg-[#292E33] text-white py-16">
+  <footer class="bg-[#292E33] text-white py-16">
     <div class="px-8 max-w-screen-xl lg:mx-auto">
       <div>
         <p class="text-xl">Prêt à échanger ?</p>
@@ -204,15 +211,34 @@
         </form>
       </div>
     </div>
-  </section>
+  </footer>
 
 </template> 
 
 <script>
 export default {
   name: 'App',
-  components: {
-  }
+  data(){
+    return {
+      isScrolling: false,
+    }
+  },
+  methods: {
+    isStillAtTop(){
+      if(window.scrollY > 870){
+        this.isScrolling=true;
+      } else {
+        this.isScrolling=false;
+      }
+    },
+    anchorPixel(){
+      window.scrollTo(window.scrollY - 300);
+    }
+  },
+  created(){
+    window.addEventListener('scroll', this.isStillAtTop);
+    window.addEventListener('hashchange', this.anchorPixel);
+  },
 }
 </script>
 
@@ -231,8 +257,14 @@ html {
   -moz-osx-font-smoothing: grayscale;
   color: #000;
 }
-h1, h2, h3, h4, h5, h6 {
-  font-family: 'Roboto', 'Montserrat', Arial, sans-serif;
+h1 {
+  font-family: 'Roboto' ,Avenir, Helvetica, Arial, sans-serif;
+}
+h2 {
+  font-family: 'Oswald' ,Avenir, Helvetica, Arial, sans-serif;
+}
+h3, h4, h5, h6 {
+  font-family: 'Montserrat' ,Avenir, Helvetica, Arial, sans-serif;
 }
 .bgafter {
   &:after {
